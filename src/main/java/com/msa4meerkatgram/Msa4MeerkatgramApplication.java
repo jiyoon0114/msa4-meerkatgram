0f93165d207c463f82a03d062d609397fa35275c
jiyoon0114 <greenpkh02@naver.com>
2026-07-06 17:27:37 +0900

    260706 JPA 전환작업중 (기본 설정 및 엔티티 작성 완료)

--- src/main/java/com/msa4meerkatgram/Msa4MeerkatgramApplication.java
+++ src/main/java/com/msa4meerkatgram/Msa4MeerkatgramApplication.java
@@ -3,13 +3,15 @@ package com.msa4meerkatgram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+// 어플리케이션 전체에 JPA Auditing 기능을 사용할테니 준비해라. 선언
+// created_at, updated_at 자동으로 채워줌
+@EnableJpaAuditing
 public class Msa4MeerkatgramApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Msa4MeerkatgramApplication.class, args);
     }
-
 }
--- src/main/java/com/msa4meerkatgram/domain/post/entities/Post.java
+++ src/main/java/com/msa4meerkatgram/domain/post/entities/Post.java
@@ -1,16 +1,59 @@
 package com.msa4meerkatgram.domain.post.entities;
 
-import lombok.Builder;
+import com.msa4meerkatgram.domain.user.entities.User;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@Entity // 해당 클래스는 DB에서 관리할 JPA 엔티티임을 선언
+// 이 엔티티에서 특정 이벤트가 발생하면 AuditingEntityListener에게 알려라
+// 그 엔티티에 이벤트가 발생할떄 AuditingEntityListener가 createdAt, updatedAt을 자동으로 넣어주고 DB 저장함
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "posts") // 테이블명 맵핑
+@SQLDelete(sql = "UPDATE posts SET deleted_at = NOW() WHERE id = ?") // delete 처리를 하고 싶을때 이 쿼리를 실행해라
+@SQLRestriction("deleted_at IS NULL") // 엔티티 조회 시 항상 특정 조건을 추가하도록 지정
 @Getter
-@Builder
+@Setter
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "BIGINT UNSIGNED")
     private Long id;
-    private Long userId;
+
+    @Column(name = "content", nullable = false, length = 200)
     private String content;
+
+    @Column(name = "image", nullable = false, length = 100)
     private String image;
-    private String createdAt;
-    private String updatedAt;
-    private String deletedAt;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at", nullable = true)
+    private LocalDateTime deletedAt;
+
+    // 현재 작성하는 entity 기준을 앞으로 (Post가 n이니 Many)
+    // 이 연관관계에서는 연관관계를 쓰겠다
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "user_id",
+            insertable = true,
+            updatable = false, // update할때 user 객체에 어떤 값을 넣더라도, update문에 user_id 칼럼을 포함하지 않겠다.
+            nullable = false,
+            // 물리적 외래키 제약조건 만들진 않음
+            foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT)
+    )
+    private User user;
 }
