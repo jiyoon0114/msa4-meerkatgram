ffc49d954b6de3b52238d93c077fd775a2d239af
jiyoon0114 <greenpkh02@naver.com>
2026-07-09 18:09:25 +0900

    260709 sprindoc OpenApi

--- src/main/java/com/msa4meerkatgram/domain/auth/responses/AuthRes.java
+++ src/main/java/com/msa4meerkatgram/domain/auth/responses/AuthRes.java
@@ -2,7 +2,9 @@ package com.msa4meerkatgram.domain.auth.responses;
 
 import com.msa4meerkatgram.domain.user.entities.User;
 import com.msa4meerkatgram.domain.user.responses.UserWithPostCountRes;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "로그인 응답")
 public record AuthRes(
     UserWithPostCountRes userWithPostCountRes,
     String accessToken
